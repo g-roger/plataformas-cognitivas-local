acc9ea69ef64d10316ff0600a70806e0170594c2
Gabriel Roger <gabriel.nroger@gmail.com>
2021-11-23 22:00:18 -0300

    Criar modelo 02

--- localdev/client2/client2.py
+++ localdev/client2/client2.py
@@ -1,18 +1,19 @@
-# Coloque aqui o código do teu cliente em Python
-
-#Passo 1:
-# Buscar de uma fila de pedidos de empréstimo
-# Fila está disponível por GET em:
-#https://us-central1-emf-teacher.cloudfunctions.net/function-aulas-getclient
-#Parâmetro qtde permite buscar mais de um cliente da fila
-# Exibir em tela os clientes da fila
+import requests
+import pandas as pd
 
 
-#Passo 2:
-# Chamar o teu micro serviço cognitivo
-# Exibir em tela a resposta do teu serviço de crédito
+if __name__ == "__main__":
+    url = requests.get('https://us-central1-emf-teacher.cloudfunctions.net/function-aulas-getclient?qtde=1000')
+    raw_data = url.json()
+    data = pd.DataFrame.from_dict(raw_data)
 
+    filtrados = data.sample(10)
 
+    url = "http://localhost:8080/modelo02"
+    headers = {'Content-Type': 'application/json'}
+    conteudo = filtrados.to_json()
 
-# Extra: Opcional
-# Gravar em arquivo CSV sequencial
+    response = requests.request("POST", url, headers=headers, data=conteudo)
+    print("Resposta da API:")
+    print(response.text.encode('utf8').decode())
+    pass
